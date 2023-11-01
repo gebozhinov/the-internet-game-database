47e8d32131756982f164a1633b1618e97e152b5e
gebozhinov <ge.bozhinov@gmail.com>
2023-11-01 16:04:10 +0200

    The Internet Game Database

---
+++ src/main/java/bg/softuni/theinternetgamedatabase/TheInternetGameDatabaseApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.theinternetgamedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheInternetGameDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheInternetGameDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/theinternetgamedatabase/TheInternetGameDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.theinternetgamedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheInternetGameDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
