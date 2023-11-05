1ad8e7f4adc9802b3716078b1e63e301c0a91064
gebozhinov <ge.bozhinov@gmail.com>
2023-11-05 19:26:11 +0200

    added login controller, login user dto and minor changes in login.html

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,11 +1,19 @@
 let loginForm = document.querySelector(".my-form");
 
+fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+        text: text
+    })
+}).then((response) => response.json());
+
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    let email = document.getElementById("email");
+    let username = document.getElementById("username");
     let password = document.getElementById("password");
 
-    console.log('Email:', email.value);
+    console.log('Username:', username.value);
     console.log('Password:', password.value);
     // process and send to API
 });
