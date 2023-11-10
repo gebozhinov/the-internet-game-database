a85b05e83c4f8e3e2236f355ac1ced925ee30b5c
gebozhinov <ge.bozhinov@gmail.com>
2023-11-10 18:46:23 +0200

    spring security dependency and password config

---
+++ src/main/java/bg/softuni/theinternetgamedatabase/model/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package bg.softuni.theinternetgamedatabase.model.mapper;
+
+import bg.softuni.theinternetgamedatabase.model.dto.RegisterUserDTO;
+import bg.softuni.theinternetgamedatabase.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+
+    @Mapping(target = "password", ignore = true)
+    User registerUserDtoToUser(RegisterUserDTO registerUserDTO);
+}
--- src/main/java/bg/softuni/theinternetgamedatabase/service/UserService.java
+++ src/main/java/bg/softuni/theinternetgamedatabase/service/UserService.java
@@ -1,6 +1,8 @@
 package bg.softuni.theinternetgamedatabase.service;
 
 import bg.softuni.theinternetgamedatabase.model.dto.RegisterUserDTO;
+import bg.softuni.theinternetgamedatabase.model.entity.User;
+import bg.softuni.theinternetgamedatabase.model.mapper.UserMapper;
 import bg.softuni.theinternetgamedatabase.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +11,18 @@ public class UserService {
 
 
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
+    private final UserMapper userMapper;
+    public UserService(UserRepository userRepository,
+                       UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
 
     public void register(RegisterUserDTO registerUserDTO) {
+
+        User user = this.userMapper.registerUserDtoToUser(registerUserDTO);
+
 
     }
 }
